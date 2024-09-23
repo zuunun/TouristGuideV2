424e9849e697f639a405bbb1bd12fc52843ff373
“zuunun” <zuun0001@stud.kea.dk>
2024-09-23 10:43:28 +0200

    First

--- src/test/java/org/example/touristguide/repository/TouristRepositoryTest.java
+++ src/test/java/org/example/touristguide/repository/TouristRepositoryTest.java
@@ -15,20 +15,41 @@ class TouristRepositoryTest {
     @Test
     void getAllAttractions() {
         List<TouristAttraction> touristAttractions = repository.getAllAttractions();
-        assertEquals(5,touristAttractions.size());
+        assertEquals(5, touristAttractions.size());
     }
 
 
     @Test
     void getAttractionByName_SUCCES() {
+        TouristAttraction attraction = repository.getAttractionByName("Tivoli Gardens");
+        assertNotNull(attraction);
+        assertEquals("Tivoli Gardens", attraction.getName());
     }
 
     @Test
     void getAttractionByName_FAIL() {
+        TouristAttraction attraction = repository.getAttractionByName("KEA");
+        assertNotNull(attraction);
     }
 
     @Test
+    void updateAttraction() {
+        TouristAttraction attraction = new TouristAttraction("LEGOLAND", "En verdensberømt temapark baseret på LEGO.", "Billund");
+        attraction.setDescription("En ny verden");
+        assertEquals("En ny verden", attraction.getDescription());
+    }
+
+
+    @Test
     void saveAttraction() {
+        TouristAttraction attraction = new TouristAttraction("Egenskiv skov", "en stor skov", "Copenhagen");
+        attraction.setTags(Arrays.asList(Tag.FAMOUS, Tag.HISTORY));
+        repository.saveAttraction(attraction);
+
+        List<TouristAttraction> t = repository.getAllAttractions();
+        assertEquals(6, t.size());
+        assertEquals("Egenskiv skov", t.get(5).getName());
+
 
     }
 
@@ -44,7 +65,5 @@ class TouristRepositoryTest {
     void getTags() {
     }
 
-    @Test
-    void updateAttraction() {
-    }
+
 }
